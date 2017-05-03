d8f3814cd5e7d1497697795ad7cfad7f68079e90
Doug Keller <dkeller@synergydatasystems.com>
2017-05-03 03:04:17 -0400

    Add command line args to foo.py

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const exec = require('child_process').exec;
-var command = 'python foo.py';
+var rootCommand = 'python foo.py ';
 
 app.set('view engine', 'ejs');
 
@@ -12,7 +16,16 @@ app.get('/', (request, response) => {
   response.render('index');
 });
 
-app.post('/', (request, response) => {
+app.post('/estimate', (request, response) => {
+  var pCount = parseInt(request.body.playercount);
+  var pLevel = parseInt(request.body.playerlevel);
+  var mCount = parseInt(request.body.monstercount);
+  var mLevel = parseInt(request.body.monsterlevel);
+
+  var args = [pCount, pLevel, mCount, mLevel].map(v => v.toString()).join(' ');
+  var command = rootCommand + args;
+
+  console.log(`Executing command: ${command}`);
   exec(command, (error, stdout, stderr) => {
     response.render('result', { text: stdout });
   });
--- foo.py
+++ foo.py
@@ -1,1 +1,10 @@
-print('Hello from Python!')
+import sys
+
+pCount = int(sys.argv[1])
+pLevel = float(sys.argv[2])
+mCount = int(sys.argv[3])
+mLevel = float(sys.argv[4])
+
+total = pCount + pLevel + mCount + mLevel
+
+print('Hello from Python! %(total)s' % { 'total': total })
