96d57639a19bee9c9d3fff8f29f260df2f9abaf4
Doug Keller <dkeller@synergydatasystems.com>
2017-05-11 13:11:48 -0400

    Store best tree afte 100 iterations of randomizing data

--- dnd/estimate.py
+++ dnd/estimate.py
@@ -25,7 +25,7 @@ class DecisionTreeData(dict):
             raise AttributeError(key)
 
     def partition(self, num, fold_index):
-        fold_size = len(self.data) / 10
+        fold_size = len(self.data) / self.folds
         start = num * fold_size
         end = start + fold_size
 
@@ -92,36 +92,48 @@ def test(case):
 if sys.argv[1] == 'build':
     overall_accuracy = []
 
-    arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    dnd_tree = load_dnd_tree()
+    best_tree = None
+    best_accuracy = 0
 
-    for i in arr:
-        res = dnd_tree.partition(i, 10)
-        tree_data = res[0]
-        tree_target = res[1]
-        test_data = res[2]
-        test_target = res[3]
+    for _ in range(100):
+        dnd_tree = load_dnd_tree()
+        dnd_tree.folds = 10
 
-        if len(tree_target) == 0 or len(test_target) == 0:
-            break
+        local_accuracy = []
 
-        classifier = tree.DecisionTreeClassifier()
-        classifier = classifier.fit(tree_data, tree_target)
-        results = classifier.predict(test_data)
-        matrix = confusion_matrix(test_target, results)
+        for i in range(dnd_tree.folds):
+            res = dnd_tree.partition(i, 10)
+            tree_data = res[0]
+            tree_target = res[1]
+            test_data = res[2]
+            test_target = res[3]
 
-        num_correct = 0
-        total = 0
+            if len(tree_target) == 0 or len(test_target) == 0:
+                break
 
-        for i, row in enumerate(matrix):
-            for j, cell in enumerate(row):
-                if i == j:
-                    num_correct += cell
+            classifier = tree.DecisionTreeClassifier()
+            classifier = classifier.fit(tree_data, tree_target)
+            results = classifier.predict(test_data)
+            matrix = confusion_matrix(test_target, results)
 
-                total += cell
+            num_correct = 0
+            total = 0
 
-        accuracy = num_correct / float(total)
-        overall_accuracy.append(accuracy)
+            for i, row in enumerate(matrix):
+                for j, cell in enumerate(row):
+                    if i == j:
+                        num_correct += cell
+
+                    total += cell
+
+            accuracy = num_correct / float(total)
+            overall_accuracy.append(accuracy)
+            local_accuracy.append(accuracy)
+
+        average_accuracy = sum(local_accuracy) / len(local_accuracy)
+        if average_accuracy > best_accuracy:
+            best_tree = dnd_tree
+            best_accuracy = average_accuracy
 
     with open('dnd/decision_tree.pkl', 'wb') as file:
         # Generate the full tree and save it
@@ -131,8 +143,7 @@ if sys.argv[1] == 'build':
         decision_tree = [dnd_tree, classifier]
         cPickle.dump(decision_tree, file)
     with open('dnd/accuracy.txt', 'w') as file:
-        accuracy = sum(overall_accuracy) / len(overall_accuracy)
-        file.write(str(accuracy))
+        file.write(str(best_accuracy))
 
 elif sys.argv[1] == 'accuracy':
     with open('dnd/accuracy.txt', 'rb') as file:
